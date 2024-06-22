f3b33b00cd8b5b44cb0c0c8140486a0bfdf09e96
Pritivi Chhabria Sachin <pritivischhabria@berkeley.edu>
2024-06-22 17:19:16 -0500

    Initial Commit

---
+++ Cuztom/CuztomApp.swift
@@ -0,0 +1,17 @@
+//
+//  CuztomApp.swift
+//  Cuztom
+//
+//  Created by Pritivi S Chhabria on 6/22/24.
+//
+
+import SwiftUI
+
+@main
+struct CuztomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
