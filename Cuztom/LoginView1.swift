5205fe58466ef42d516cc8e02ff5aaa96e9a1b0b
Pritivi Chhabria <cspritivi@gmail.com>
2024-07-05 00:18:43 -0500

    Added some login views

---
+++ Cuztom/LoginView1.swift
@@ -0,0 +1,28 @@
+//
+//  LoginView.swift
+//  Cuztom
+//
+//  Created by Pritivi S Chhabria on 7/4/24.
+//
+
+import SwiftUI
+
+struct LoginView1: View {
+    var body: some View {
+        VStack {
+            Image("fullFormLogo")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 500, height: 400)
+            
+            Button("Get started") {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
+            
+        }
+    }
+}
+
+#Preview {
+    LoginView1()
+}
---
+++ Cuztom/LoginView2.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView2.swift
+//  Cuztom
+//
+//  Created by Pritivi S Chhabria on 7/5/24.
+//
+
+import SwiftUI
+
+struct LoginView2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView2()
+}
