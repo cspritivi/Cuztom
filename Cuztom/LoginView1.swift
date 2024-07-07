879f72c57b0e63d4e743d59e102070e082db7178
Pritivi Chhabria <cspritivi@gmail.com>
2024-07-07 16:37:01 -0500

    Created bare bones login page with buttons

--- Cuztom/LoginView1.swift
+++ Cuztom/LoginView1.swift
@@ -15,9 +15,16 @@ struct LoginView1: View {
                 .scaledToFit()
                 .frame(width: 500, height: 400)
             
-            Button("Get started") {
+            Button("Create A New Account") {
                 /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
             }
+            .padding(10)
+            .buttonStyle(.bordered)
+            
+            Button("Sign In With An Existing Account"){
+                
+            }
+            .buttonStyle(.bordered)
             
         }
     }
