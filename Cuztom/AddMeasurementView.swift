1fe9b80736e228b3a1afaf40f7e8015ab9d1d9cc
Pritivi Chhabria <cspritivi@gmail.com>
2024-09-07 05:06:21 -0500

    Added some forms

--- Cuztom/AddMeasurementView.swift
+++ Cuztom/AddMeasurementView.swift
@@ -8,11 +8,51 @@
 import SwiftUI
 
 struct AddMeasurementView: View {
+    
+    @EnvironmentObject var authViewModel: AuthViewModel
+    
+    private let selectionOptions = ["Shirt", "Pant"]
+    @State private var selected = ""
+    @State private var forSelf = true
+    @State private var measurementFor = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        measurementFor = authViewModel.currentUser?.fullName ?? "Not logged in"
+//        guard  let user = authViewModel.currentUser else { return Text("No1 logged in") }
+        return Form {
+            Picker("Measurement Type", selection: $selected) {
+                ForEach(selectionOptions, id:\.description) {
+                    Text($0)
+                }
+            }
+            .pickerStyle(.inline)
+            
+            Section("This Measure Is") {
+                Toggle(isOn: $forSelf) {
+                    Text("For Myself")
+                    Text("Switch this off only if these measurements are someone else's")
+                }
+                .toggleStyle(.switch)
+                TextField("Measurment For:", text: $measurementFor, prompt: Text("Full Name"))
+                    .disabled(forSelf)
+            }
+            
+        }
+        .navigationTitle("Add New Measurment")
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            ToolbarItem(placement: .topBarTrailing) {
+                NavigationLink(value: "MeasurementDetails") {
+                    Text("Next")
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    AddMeasurementView()
+    NavigationStack {
+        AddMeasurementView()
+            .environmentObject(AuthViewModel())
+    }
 }
