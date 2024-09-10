c714dbb08d799ff70d117f44257e769677f0d5f9
Pritivi Chhabria <cspritivi@gmail.com>
2024-09-10 01:30:27 -0500

    The form for adding measurements in done in principle, need to add backend support now

--- Cuztom/MeasurementFormView.swift
+++ Cuztom/MeasurementFormView.swift
@@ -11,6 +11,7 @@ struct MeasurementFormView: View {
     
     let detailsNeeded:[String]
     @State var values:[String]
+    @FocusState private var focusedField: Int?
     
     init() {
         self.detailsNeeded = ShirtMeasurement.detailsNeeded
@@ -23,16 +24,66 @@ struct MeasurementFormView: View {
                 ForEach(0..<detailsNeeded.count, id:\.self) { index in
                     InputView(text: self.$values[index], title: self.detailsNeeded[index], placeholder: "Enter Details")
                         .keyboardType(.decimalPad)
-                        .submitLabel(.next)
-                        .onSubmit {
-                            print("formSubmited")
-                        }
+                        .focused($focusedField, equals: index)
                 }
             }
         }
+        .toolbar {
+            ToolbarItemGroup(placement: .keyboard) {
+                Spacer()
+                HStack {
+                    Button(action: moveToPreviousField) {
+                        Image(systemName: "chevron.up")
+                    }
+                    .disabled(!canMoveToPreviousField)
+                    
+                    Button(action: moveToNextField) {
+                        Image(systemName: "chevron.down")
+                    }
+                    .disabled(!canMoveToNextField)
+                    
+                    Button {
+                        focusedField = nil
+                    } label: {
+                        Image(systemName: "checkmark")
+                    }
+
+                }
+            }
+        }
+        .navigationTitle("New Shirt Measurement")
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar(content: {
+            Button("Done") {
+                
+            }
+            .disabled(!(self.values.filter {$0 == ""}.count == 0))
+        })
+    }
+    
+    private func moveToPreviousField() {
+        guard let currentFocus = focusedField, currentFocus > 0 else { return }
+        focusedField = currentFocus - 1
+    }
+    
+    private var canMoveToPreviousField: Bool {
+        guard let currentFocus = focusedField else { return false }
+        return currentFocus > 0
+    }
+    
+    private func moveToNextField() {
+        guard let currentFocus = focusedField, currentFocus < self.detailsNeeded.count - 1 else { return }
+        focusedField = currentFocus + 1
+    }
+    
+    private var canMoveToNextField: Bool {
+        guard let currentFocus = focusedField else { return false }
+        return currentFocus < self.detailsNeeded.count - 1
     }
 }
 
 #Preview {
-    MeasurementFormView()
+    NavigationStack {
+        MeasurementFormView()
+    }
 }
