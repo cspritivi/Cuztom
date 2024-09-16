ccdeb34358e2b8680fd537d8009b763b5b603dad
Pritivi Chhabria <cspritivi@gmail.com>
2024-09-16 14:30:55 -0500

    Completed Basic Interface and Persistence of Measurement, tests need to be added

---
+++ Cuztom/InfoRowView.swift
@@ -0,0 +1,18 @@
+//
+//  InfoRowView.swift
+//  Cuztom
+//
+//  Created by Pritivi S Chhabria on 9/16/24.
+//
+
+import SwiftUI
+
+struct InfoRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InfoRowView()
+}
---
+++ Cuztom/MeasurementViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MeasurementViewModel.swift
+//  Cuztom
+//
+//  Created by Pritivi S Chhabria on 9/15/24.
+//
+
+import Foundation
