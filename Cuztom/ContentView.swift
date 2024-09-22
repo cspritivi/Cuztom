25e13dedf971177183a448687b5ca8aca0238fc7
Pritivi Chhabria <cspritivi@gmail.com>
2024-09-22 11:07:06 -0500

    Added UI for TabView which is now synchronously loading all the views, but the views still need to be fleshed out

--- Cuztom/ContentView.swift
+++ Cuztom/ContentView.swift
@@ -14,7 +14,28 @@ struct ContentView: View {
     var body: some View {
         Group {
             if authViewModel.userSession != nil {
-                ProfileView()
+                TabView {
+                    HomeView()
+                        .tabItem {
+                            Image(systemName: "house.fill")
+                            Text("Home")
+                        }
+                    OrderView()
+                        .tabItem {
+                            Image(systemName: "plus.app.fill")
+                            Text("Order")
+                        }
+                    FavoritesView()
+                        .tabItem {
+                            Image(systemName: "suit.heart.fill")
+                            Text("Saved")
+                        }
+                    ProfileView()
+                        .tabItem {
+                            Image(systemName: "person.crop.circle.fill")
+                            Text("Profile")
+                        }
+                }
             } else {
                 LoginView1()
             }
---
+++ Cuztom/OrderView.swift
@@ -0,0 +1,18 @@
+//
+//  NewOrderView.swift
+//  Cuztom
+//
+//  Created by Pritivi S Chhabria on 9/21/24.
+//
+
+import SwiftUI
+
+struct OrderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OrderView()
+}
